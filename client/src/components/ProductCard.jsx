a959942febb5f86114b0be99a292389c8e051921
Christopher d'Arcy <cdarcy656481@gmail.com>
2025-04-30 13:50:52 -0400

    add fallback image for product card component

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardContent, CardTitle } from './ui/card';
 
+const DEFAULTIMAGE = '/default-image-1.jpg';
+
 function ProductCard({ product }) {
+  const [productImg, setProductImg] = useState(product.image);
+
   return (
     <Card>
       <div className="flex justify-between px-1.5">
@@ -12,7 +16,11 @@ function ProductCard({ product }) {
         <p className="text-xl font-bold">${product.price}</p>
       </div>
       <CardContent>
-        <img src={product.image} alt="product" />
+        <img
+          src={productImg}
+          alt="product"
+          onError={() => setProductImg(DEFAULTIMAGE)}
+        />
         <CardTitle className="text-3xl font-extrabold">
           {product.name}
         </CardTitle>
